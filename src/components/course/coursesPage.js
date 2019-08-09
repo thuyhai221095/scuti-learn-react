31fba46848c1926251cdc6a0b7dc72980d0c0ac6
thuylee <thuylee221095@gmail.com>
2019-08-09 18:09:46 +0700

    init commit

---
+++ src/components/course/coursesPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class coursesPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Courses</h1>
+      </div>
+    );
+  }
+}
+
+export default coursesPage;
